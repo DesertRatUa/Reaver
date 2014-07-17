94fbc78704d1a5a178f73e553a46446d32967603
desert rat <desert rat@dc3100d2-2716-41d1-9c91-34948b7458d4>
2014-07-17 15:50:46 +0000

    git-svn-id: https://devnull.biz.ua/svn/reaver@13 dc3100d2-2716-41d1-9c91-34948b7458d4

--- CommunicationManager.h
+++ CommunicationManager.h
@@ -30,19 +30,31 @@ public:
 	void Close();
 
 protected:
+	struct SocketData
+	{
+		SocketData( CommunicationManager *Manager, pthread_t &Thread );
+		CommunicationManager *manager;
+		SOCKET socket;
+		pthread_t *thread;
+		sockaddr_in addr;
+	};
+	typedef std::vector<pthread_t> ThreadsList;
+
 	void Update();
 	static void *Run( void *arg );
+	static void *ConnectionThread( void *arg );
 
-	SOCKET &CreateConnectedSocket( const SOCKET &socket );
-	void CloseConnectedSocket( const SOCKET &socket );
+	pthread_t& AddThread();
+	void RemoveThread( const pthread_t *thread );
 
 	bool m_run;
 	WSADATA m_wsaData;
 	SOCKET m_socket;
-	typedef std::vector<SOCKET> Sockets;
-	Sockets m_connected;
 	pthread_t m_mainThread;
 	timeval m_timeout;
+	pthread_mutex_t m_threadList;
+
+	ThreadsList m_listenThreads;
 };
 
 #endif /* COMMUNICATIONMANAGER_H_ */
